b6605ab095bc631efaa4825d36a8d53cbc3d20c5
uriel <ualvarez95@gmail.com>
2021-04-30 21:11:04 -0300

    angula cli install

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The USA-Turnos App';
+  users: any;
+
+  ngOnInit(){
+   this.getUsers();
+  }
+
+getUsers(){
+  this.http.get('https://localhost:5001/api/users').subscribe(response => {
+    this.users = response;
+  }, error => {
+    console.log(error);
+  })
+}
+
+  constructor(private http: HttpClient) { }
+}
